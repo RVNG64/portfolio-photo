aa75b57815a8e299a51ce47576abf89567eefcf2
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-05-07 00:25:02 +0200

    design page contact
    Signed-off-by: HervÃe Nguetsop <herve.nguetsop@gmail.com>

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Projects = () => {
-  return <h1>Galerie</h1>;
+  return (
+    <div>
+      <h1>Galerie de projets</h1>
+      <p>
+        Voici une sélection de mes projets photographiques les plus récents et les plus marquants. Cliquez sur un projet pour en savoir plus et pour voir la galerie complète.
+      </p>
+      {/* Ajouter ici des cartes de projet ou des images avec des liens vers les projets individuels. */}
+    </div>
+  );
 };
 
 export default Projects;
