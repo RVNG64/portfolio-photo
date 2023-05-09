1e752a6924429321fa52fb7d2d97ebcc754cebd8
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-05-09 16:37:13 +0200

    Suppression page about et dépendances
    Signed-off-by: HervÃe Nguetsop <herve.nguetsop@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
@@ -13,7 +12,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import PhotoMosaic from 'components/PhotoMosaic';
 
 const Home = () => {
   return (
     <>
       <Header />
+      <PhotoMosaic />
     </>
   );
 };
